22531ac2b52b6d58b4da76aee7e9e4b71dcc2bc0
Mmoussier <morgann.moussier@free.fr>
2019-04-27 10:02:10 +0200

    Suppression commentaires tests Git

--- src/TypesDonnees/DecimalFormatTest.java
+++ src/TypesDonnees/DecimalFormatTest.java
@@ -8,12 +8,6 @@ public class DecimalFormatTest
 {
     public static void main( String[] args )
     {
-        // branch-1
-        // branch-2
-        // master 27/04-1
-        // master 27/04-2
-        // master 27/04-3 (Dell-Morgann)
-        // master 27/04-4 (Dell-Morgann)
         Double test1 = 123.456;
 
         // Provoque une erreur NumberFormatException avec les paramètres régionaux français
@@ -25,8 +19,5 @@ public class DecimalFormatTest
         Double test = Double.parseDouble(decimalFormat.format(test1));
 
         System.out.println( "test : " +test );
-
-        // master 27/04-4 Morgannm-e6220
-        // master 27/04-5 Morgannm-e6220
     }
 }
