a6c6ca0fa1c72a43df8696e16be236a631070ce4
Mmoussier <morgann.moussier@free.fr>
2020-10-09 09:38:13 +0200

    Format numbers

---
+++ src/TypesDonnees/FormatNumbers.java
@@ -0,0 +1,17 @@
+package TypesDonnees;
+
+public class FormatNumbers {
+    public static void main(String[] args) {
+        Double test1 = 1.234;
+        Double test2 = 1000.0;
+        Double result1 = test2/(test1*1000);
+        System.out.println(result1);
+        System.out.println(String.format("%f",result1));
+
+        Double test3 = 2.234;
+        Double test4 = 1.0;
+        Double result2 = test4/(test3*1000);
+        System.out.println(result2);
+        System.out.println(String.format("%.4f",result2));
+    }
+}
