7ce167d4c78f14db166cdb296c596867c0f1a2a9
Mmoussier <morgann.moussier@free.fr>
2021-08-29 10:01:24 +0200

    Nombre de jours entre 2 dates

--- src/JavaTime/PeriodDurationDemo.java
+++ src/JavaTime/PeriodDurationDemo.java
@@ -1,10 +1,12 @@
 package JavaTime;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.Period;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class PeriodDurationDemo {
     public static void main(String[] args) {
@@ -19,5 +21,29 @@ public class PeriodDurationDemo {
         System.out.println("Durée : " + d.getSeconds());
         System.out.println("Ecart en jour : " + ChronoUnit.DAYS.between(ldt, ldt2));
 
+        // Différence entre 2 dates LocalDate (autre méthode)
+        System.out.println("Différence entre 2 dates LocalDate (autre méthode) --------------");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MM yyyy");
+        String inputString1 = "01 01 1997";
+        String inputString2 = "23 01 1997";
+
+        LocalDate date1 = LocalDate.parse(inputString1, dtf);
+        LocalDate date2 = LocalDate.parse(inputString2, dtf);
+        long daysBetween = ChronoUnit.DAYS.between(date1, date2);
+        System.out.println ("Days: " + daysBetween);
+
+        // Différence entre 2 dates Date converties en LocalDate
+        System.out.println("Différence entre 2 dates Date converties en LocalDate --------------");
+        SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
+        try {
+            Date date3 = myFormat.parse(inputString1);
+            Date date4 = myFormat.parse(inputString2);
+            ldt3 = LocalDateTime.ofInstant(date3.toInstant(), ZoneId.systemDefault());
+            LocalDateTime ldt4 = LocalDateTime.ofInstant(date4.toInstant(), ZoneId.systemDefault());
+            daysBetween = Duration.between(ldt3,ldt4).toDays();
+            System.out.println ("Days: " + daysBetween);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
