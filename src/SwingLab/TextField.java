b23a193a8adb4e5b327067a5f207937372af711c
Mmoussier <morgann.moussier@free.fr>
2023-03-25 12:25:17 +0100

    Test Focus sur TextField

--- src/SwingLab/TextField.java
+++ src/SwingLab/TextField.java
@@ -1,6 +1,8 @@
 package SwingLab;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class TextField extends JFrame {
     public TextField() {
@@ -9,18 +11,42 @@ public class TextField extends JFrame {
         setSize(600, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        JLabel pageLabel = new JLabel("Web page address: ", JLabel.RIGHT);
 
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new FlowLayout(FlowLayout.LEFT));
+        JLabel pageLabel = new JLabel("Web page address: ", JLabel.RIGHT);
+        panel1.add(pageLabel);
         JTextField pageAddress = new JTextField("Valeur par défaut");
         pageAddress.setPreferredSize(new Dimension(400,30));
         pageAddress.setForeground(Color.BLUE);
+        pageAddress.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                System.out.println("focusGained");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                System.out.println("focusLost");
+            }
+        });
         Font police = new Font("Arial", Font.BOLD, 14);
         pageAddress.setFont(police);
+        panel1.add(pageAddress);
+        mainPanel.add(panel1);
 
-        FlowLayout flo = new FlowLayout();
-        setLayout(flo);
-        add(pageLabel);
-        add(pageAddress);
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout(FlowLayout.LEFT));
+        panel2.add(new JLabel("Test"));
+        JTextField textField2 = new JTextField();
+        textField2.setPreferredSize(new Dimension(400,30));
+        panel2.add(textField2);
+        mainPanel.add(panel2);
+
+        add(mainPanel);
         setVisible(true);
     }
     
