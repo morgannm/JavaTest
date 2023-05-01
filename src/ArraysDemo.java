8a612bfc8ef8596b248726da82586117e6b2015b
Mmoussier <morgann.moussier@free.fr>
2023-05-01 09:06:52 +0200

    Tableau d'objets

--- src/ArraysDemo.java
+++ src/ArraysDemo.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class ArraysDemo {
     public static void main(String[] args) {
+
         // Tri d'un tableau avec la méthode sort de la classe Arrays
         String names[] = { "Lauren", "Audrina", "Heidi", "Whitney",
             "Stephanie", "Spencer", "Lisa", "Brody", "Frankie",
@@ -16,5 +17,45 @@ class ArraysDemo {
             System.out.print(i + ": " + names[i] + " ");
         }
         System.out.println();
+
+        // Tableau d'objets
+        Player[] players = new Player[5];
+        players[0] = new Player("Connor","Mc David");
+        players[1] = new Player("David","Pastrnak");
+        players[2] = new Player("Auston","Matthews");
+        players[3] = new Player("Cale","Makar");
+        players[4] = new Player("Artemi","Panarin");
+        for (int i=0; i<=4;i++) System.out.println(players[i].toString());
+    }
+
+    private static class Player {
+        private String firstName;
+        private String lastName;
+
+        public Player(String firstName, String lastName) {
+            this.firstName=firstName;
+            this.lastName=lastName;
+        }
+
+        @Override
+        public String toString() {
+            return this.firstName + " " + this.lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
     }
 }
