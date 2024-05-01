ca10bbbb086f887975a9fd2cf1e538b50e4ab6c6
Mmoussier <morgann.moussier@free.fr>
2024-05-01 16:05:30 +0200

    Arrondi inférieur

--- src/TypesDonnees/Arrondis.java
+++ src/TypesDonnees/Arrondis.java
@@ -14,5 +14,11 @@ public class Arrondis {
         qteInitiale=1501.0;
         qteArrondie = (Math.ceil(qteInitiale/qteParPaquet))*qteParPaquet;
         System.out.println("Qté initiale : " + qteInitiale + " => Qté arrondie : " + qteArrondie);
+
+        //
+        float x1 = 90;
+        float x2 = 59;
+        int nb = (int) Math.floor(x1/x2);
+        System.out.println("Math.floor(" + x1 + "/" + x2 + ") = " + nb);
     }
 }
